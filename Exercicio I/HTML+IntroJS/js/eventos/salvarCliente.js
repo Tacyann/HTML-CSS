bc53b115d22f56f4891ea453fc64ad7b28fabcbc
Tacyann <tacyblpimentel@gmail.com>
2020-09-07 19:28:20 -0300

    HTML+IntroJS

---
+++ Exercicio I/HTML+IntroJS/js/eventos/salvarCliente.js
@@ -0,0 +1,12 @@
+var clientes = [];
+
+function salvarCliente() {
+
+    let cliente = {
+        nome: document.getElementById("inputNome").value,
+        cpf: document.getElementById("inputCpf").value,
+        dataNascimento: document.getElementById("inputDataNascimrnto").value
+    }
+
+    clientes.push(cliente);
+}
