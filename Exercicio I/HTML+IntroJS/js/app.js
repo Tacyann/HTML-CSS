541daf647a30fdf0d70c1d4f1550985a288198c2
Tacyann <tacyblpimentel@gmail.com>
2020-09-08 23:17:14 -0300

    HTML+IntroJS

--- Exercicio I/HTML+IntroJS/js/app.js
+++ Exercicio I/HTML+IntroJS/js/app.js
@@ -3,7 +3,7 @@ function formatarDivBody() {
     body.style.position = "absolute";
     body.style.top = "10px";
     body.style.left = "10px";
-    body.style.width = "800px";
+    body.style.width = "1450px";
     body.style.height = "400px";
     body.style.border = " 1px solid";
     body.style.borderColor = "#DCDCDC";
@@ -15,8 +15,18 @@ formatarDivBody();
 function criarComponentes() {
 
     criarDivParaInput("Nome", "10px", "10px");
-    criarDivParaInput("CPF", "10px", "250px");
-    criarDivParaInput("DataNascimento", "50px", "10px");
+    criarDivParaInput("CPF", "10px", "380px");
+    criarDivParaInput("Data de Nascimento", "10px", "680px");
+    criarDivParaInput("Endereco", "50px", "10px");
+    criarDivParaInput("Bairro", "50px", "250px");
+    criarDivParaInput("CEP", "50px", "480px");
+    criarDivParaInput("Data de Cadastro", "50px", "700px");
+    criarDivParaInput("Municipio", "85px", "10px");
+    criarDivParaInput("Telefone", "85px", "270px");
+    criarDivParaInput("Celular", "85px", "520px");
+    criarDivParaInput("UF", "85px", "750px");
+    criarDivParaInput("Inscricao Estadual", "85px", "950px");
+    criarDivParaInput("UF", "85px", "1250px");
 
     criarDivParaButton("Salvar", "200px", "10px", "salvarCliente");
     criarDivParaButton("Cliente no Console", "200px", "100px", "imprimirClientesNoConsole");
